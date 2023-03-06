cd670ec22020bc931cbeff4431254f4ea1a630f7
Gofishfly <gyf19930617533@163.com>
2023-03-06 23:35:57 +0800

    1

--- kubejs/client_scripts/tooltip.js
+++
@@ -1,6 +0,0 @@
-onEvent('item.tooltip', tooltip => {
-    tooltip.add(['#kubejs:banned_tools'], '§c这个物品只能用于合成！')
-    
-    tooltip.add(['tazsponsors:one_dollar'], '§b赞助者物品 By 新世纪鸽子战士')
-    tooltip.add(['tazsponsors:milans_sword'], '§b赞助者物品 By 画中人')
-})
--- kubejs/server_scripts/sponsor_items_events.js
+++
@@ -1,6 +0,0 @@
-onEvent('item.right_click', event => {
-    if (event.item.id == 'tazsponsors:one_dollar') {
-        event.item.count--;
-        event.player.giveInHand('32x kubejs:data');
-    }
-})
